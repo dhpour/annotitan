d78c98e8563219f7cf8a2adae944cd6cd5c274a7
dhpour <chandracar@gmail.com>
2024-08-04 20:32:33 +0330

    doc: wrote source doc of a feature

--- asrann/token.py
+++ asrann/token.py
@@ -7,3 +7,5 @@ class TokenGenerator(PasswordResetTokenGenerator):
             six.text_type(user.is_active)  
         )  
 account_activation_token = TokenGenerator()  
+
+#from https://www.javatpoint.com/django-user-registration-with-email-confirmation
